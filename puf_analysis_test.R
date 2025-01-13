d5560c0f4ae72e73e2b88de5466118b390331593
ridgleyknapp <ridge.knapp.pol@gmail.com>
2025-01-13 12:46:17 -0600

    pull demo by question input

--- puf_analysis_test.R
+++ puf_analysis_test.R
@@ -1,19 +1,30 @@
-#install.packages("autumn")
-
+library(survey)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(data.table)
+`%nin%` <- Negate(`%in%`)
 
 setwd("/Users/yehyland/iCloud Drive (Archive)/Desktop/RPK/githublink/y2025/y2025")
 
 df <- read.csv("APNORC_june2021_PUF.csv")
 
-table(df$CUR1, df$POLITICS)
+methodological <- c("SU_ID","FINALWT")
+variables <- c("POLITICS","DEMO","REPUB","INDEP","IDEO","URBAN","MARITAL","AGEGRP","EDUCATION","RACETH","GENDER","HHINCOME","EMPSTATUS","CENSUS_REGION","SURV_MODE","SURV_LANG")
+questions <- colnames(df)[!(colnames(df) %in% variables)&!(colnames(df) %in% methodological)&colnames(df)!="STATE"]
 
-df$CUR1_by_POL <- paste(df$POLITICS, " - ", df$CUR1)
+question <- c("CURY2D")
 
-ggplot(df, aes(fill=CUR1, y = FINALWT, x = ifelse(POLITICS %like% "VOL", "(5) Missing", POLITICS))) +
-         geom_bar(position = "fill", stat = "identity") +
+lapply(question,function(question) {
+  
+    for (varx in variables) {
+      print(varx)
+      print(
+        ggplot(df, aes(fill=df[[question]], y = FINALWT, x = ifelse(df[[varx]] == toupper(df[[varx]]), "(5) Missing", df[[varx]]))) +
+          geom_bar(position = "fill", stat = "identity") +
           theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  labs(x = "POLITICS", y = "PROPORTION")
+          #coord_flip() +
+          labs(x = paste(varx), y = "PROPORTION", fill = "Response") 
+      )
+    }
+})
