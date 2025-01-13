dcf8a052de971cc940696149ac91b122599e95bd
ridgleyknapp <ridge.knapp.pol@gmail.com>
2025-01-12 22:45:49 -0600

    create bar graph of politics x direction of countr

--- puf_analysis_test.R
+++ puf_analysis_test.R
@@ -1,7 +1,19 @@
+#install.packages("autumn")
+
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(data.table)
 
 setwd("/Users/yehyland/iCloud Drive (Archive)/Desktop/RPK/githublink/y2025/y2025")
 
-df <- read.csv()
+df <- read.csv("APNORC_june2021_PUF.csv")
+
+table(df$CUR1, df$POLITICS)
+
+df$CUR1_by_POL <- paste(df$POLITICS, " - ", df$CUR1)
+
+ggplot(df, aes(fill=CUR1, y = FINALWT, x = ifelse(POLITICS %like% "VOL", "(5) Missing", POLITICS))) +
+         geom_bar(position = "fill", stat = "identity") +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  labs(x = "POLITICS", y = "PROPORTION")
